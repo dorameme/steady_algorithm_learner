a19129ecf3dc84c3edb9b5f48b9c22d071ef9c5d
김연수 <68580993+dorameme@users.noreply.github.com>
2023-09-07 09:20:52 +0900

    Update 큰 수 만들기(lv2).cpp

--- 프로그래머스/큰 수 만들기(lv2).cpp
+++ 프로그래머스/큰 수 만들기(lv2).cpp
@@ -1,3 +1,11 @@
+
+/*
+start:9:10
+end:9:19
+시간복잡도:O(N*K)
+공간복잡도:O(N)
+풀이:구현문제인데 앞에서 K+1개 까지만 확인해주면 된다는게 포인트. 그리고 마지막에 K를 전부 삭제하지못했으면 뒷부분에서 남은 양만큼 제거해 줘야함.
+*/
 #include <string>
 #include <vector>
 #include <iostream>
